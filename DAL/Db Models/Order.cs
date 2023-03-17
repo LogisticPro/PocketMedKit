12fe7e0b90befd424d8feea9fbddbc214015c816
LogisticPro <logisticpro3000@gmail.com>
2023-03-17 12:10:58 +0100

    Add Db Models + classes library

---
+++ DAL/Db Models/Order.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Db_Models
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public int PharmacyId { get; set; }
+
+        [Required]
+        public double? Price { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        public string? Description { get; set; }
+
+        public virtual User User { get; set; }
+        public virtual Pharmacy Pharmacy { get; set; }
+        public virtual ICollection<Medicine> Medicines { get; set; }
+    }
+}
---
+++ DAL/Db Models/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Db_Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int PocketId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string? Name { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string? Username { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string? Password { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string? Email { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; }
+        public virtual Pocket Pocket { get; set; }
+    }
+}
