970f7562bfad67b1cec16236856bc4c15e839ac9
YahoRu <rubanyahor@gmail.com>
2023-03-30 13:20:16 +0300

    Add Dependences from main App to DAL

--- DAL/Db Models/PocketMedKitAppDbContext.cs
+++ DAL/Db Models/PocketMedKitAppDbContext.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
+using Microsoft.Data.SqlClient;
 
 namespace DAL.Db_Models
 {
@@ -19,10 +21,20 @@ namespace DAL.Db_Models
         public DbSet<Pharmacy> Pharmacies { get; set; }
         public DbSet<PocketMedicine> PocketMedicines { get; set; }
 
+        public PocketMedKitAppDbContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        public PocketMedKitAppDbContext(DbContextOptions<PocketMedKitAppDbContext> options) 
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             string? connString = _configuration.GetConnectionString("ConnectionString1");
-
             optionsBuilder.UseSqlServer(connString);
         }
     }
